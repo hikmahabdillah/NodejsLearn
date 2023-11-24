31804a6fa2c3495e84cddfc8d363e1bccfd6f666
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-11-25 00:27:04 +0700

    latihan jest / test

---
+++ latihantesting/index.js
@@ -0,0 +1,19 @@
+// test('deskripsi dari testcase kamu', () => {
+//   expect(perintah).matcher(nilai yang diekspektasikan);
+// });
+
+// contoh
+test("dua tambah dua adalah empat", () => {
+  expect(2 + 2).toBe(4);
+});
+
+// contoh lain
+// describe('pengujian olah aritmatika', () => {
+//   test('#1 dua tambah dua adalah empat', () => {
+//        expect(2+2).toBe(4);
+//   });
+
+//   test('#2 dua kali tiga adalah enam', () => {
+//        expect(2*3).toBe(6);
+//   });
+// });
